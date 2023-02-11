e893c965232706c4aefbca91137faa151cf59599
Usama Bajwa <usamabajwa86@gmail.com>
2023-02-11 22:58:46 +0500

    Update st.py

--- day35/st.py
+++ day35/st.py
@@ -4,7 +4,6 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-import pandas_profiling
 import warnings
 warnings.filterwarnings('ignore') # to ignore warnings
 
