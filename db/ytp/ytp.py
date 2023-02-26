7f49ea539ba6082232f3ff0c3df1843bcae7e064
Usama Bajwa <usamabajwa86@gmail.com>
2023-02-27 02:03:43 +0500

    Update ytp.py

--- db/ytp/ytp.py
+++ db/ytp/ytp.py
@@ -1,6 +1,6 @@
 import os
 import streamlit as st
-from pytube import Playlist, Stream
+from pytube import Playlist
 import base64
 
 def download_video(stream, title):
@@ -12,14 +12,14 @@ def download_video(stream, title):
 
 def download_all_videos(playlist_url, resolution):
     playlist = Playlist(playlist_url)
-    downloaded_videos = []
+    file_paths = []
     with st.spinner(f'Downloading {playlist.title}...'):
         for video in playlist.videos:
             stream = video.streams.filter(res=resolution).first()
             if stream:
                 file_path = download_video(stream, video.title)
-                downloaded_videos.append({'title': video.title, 'file_path': file_path})
-    return downloaded_videos
+                file_paths.append(file_path)
+    return file_paths
 
 st.title('YouTube Playlist Downloader')
 
@@ -39,18 +39,19 @@ resolutions = [
 resolution = st.selectbox('Select video resolution:', [res['label'] for res in resolutions])
 
 if st.button('Download All Videos'):
-    downloaded_videos = download_all_videos(playlist_url, resolution)
+    file_paths = download_all_videos(playlist_url, resolution)
     st.success('All videos downloaded successfully!')
 
-if downloaded_videos:
+if file_paths:
     st.write('Downloaded videos:')
-    for video in downloaded_videos:
-        if os.path.exists(video['file_path']):
-            with open(video['file_path'], 'rb') as f:
-                video_bytes = f.read()
-            st.video(video['file_path'])
-            b64 = base64.b64encode(video_bytes).decode()
-            href = f'<a href="data:file/mp4;base64,{b64}" download="{video["title"]}.mp4">Download {video["title"]}</a>'
+    video_bytes = []
+    for file_path in file_paths:
+        if os.path.exists(file_path):
+            with open(file_path, 'rb') as f:
+                video_bytes.append(f.read())
+            st.video(video_bytes[-1])
+            b64 = base64.b64encode(video_bytes[-1]).decode()
+            href = f'<a href="data:file/mp4;base64,{b64}" download="{os.path.basename(file_path)}">Download {os.path.basename(file_path)}</a>'
             st.markdown(href, unsafe_allow_html=True)
         else:
-            st.warning(f'{video["title"]} does not exist.')
+            st.warning(f'{file_path} does not exist.')
