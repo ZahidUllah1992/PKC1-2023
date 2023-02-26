fa3727b6674f119644ca87716cb0dcfbe07a0715
Usama Bajwa <usamabajwa86@gmail.com>
2023-02-27 01:51:13 +0500

    Update ytp.py

--- db/ytp/ytp.py
+++ db/ytp/ytp.py
@@ -1,63 +1,33 @@
 import os
-import streamlit as st
-from pytube import Playlist, Stream
 import base64
+import streamlit as st
+from pytube import YouTube
 
 def download_file(stream, fmt):
-    """Downloads a video or audio stream and generates a download link."""
     if fmt == 'audio':
-        title = stream.title + ' audio.'+ stream_final.subtype
+        title = stream.title + ' audio.' + stream_final.subtype
     else:
-        title = stream.title + '.'+ stream_final.subtype
+        title = stream.title + '.' + stream_final.subtype
 
     stream.download(filename=title)
     
-    if 'DESKTOP_SESSION' not in os.environ:
-        with open(title, 'rb') as f:
-            bytes = f.read()
-            b64 = base64.b64encode(bytes).decode()
-            href = f'<a href="data:file/zip;base64,{b64}" download=\'{title}\'>\
-                Here is your link \
-            </a>'
-            st.markdown(href, unsafe_allow_html=True)
+    return title
 
-        os.remove(title)
+def serve_file(file_path):
+    with open(file_path, 'rb') as f:
+        bytes = f.read()
+        b64 = base64.b64encode(bytes).decode()
+        href = f'<a href="data:file/zip;base64,{b64}" download=\'{os.path.basename(file_path)}\'>\
+            Here is your link \
+        </a>'
+        st.markdown(href, unsafe_allow_html=True)
 
-def download_all_videos(playlist_url, resolution):
-    """Downloads all videos in a playlist with the given resolution."""
-    playlist = Playlist(playlist_url)
-    downloaded_videos = []
-    with st.spinner(f'Downloading {playlist.title}...'):
-        for video in playlist.videos:
-            stream = video.streams.filter(res=resolution).first()
-            if stream:
-                download_file(stream, 'video')
-                downloaded_videos.append(video.title)
-                st.write(f'Download link for {video.title}:')
-    return downloaded_videos
-
-st.title('YouTube Playlist Downloader')
-
-playlist_url = st.text_input('Enter the URL of the YouTube playlist:')
-if not playlist_url.startswith('https://www.youtube.com/playlist?'):
-    st.warning('Please enter a valid YouTube playlist URL.')
-    st.stop()
-
-resolutions = [
-    {'label': '720p', 'value': '720p'},
-    {'label': '480p', 'value': '480p'},
-    {'label': '360p', 'value': '360p'},
-    {'label': '240p', 'value': '240p'},
-    {'label': '144p', 'value': '144p'}
-]
-
-resolution = st.selectbox('Select video resolution:', [res['label'] for res in resolutions])
-
-if st.button('Download All Videos'):
-    downloaded_videos = download_all_videos(playlist_url, resolution)
-    st.success('All videos downloaded successfully!')
-
-if downloaded_videos:
-    st.write('Downloaded videos:')
-    for video in downloaded_videos:
-        st.write(video)
+def download_and_serve_video(url, resolution):
+    yt = YouTube(url)
+    video = yt.streams.filter(res=resolution).first()
+    if video:
+        file_path = download_file(video, 'video')
+        serve_file(file_path)
+        st.success('Video downloaded successfully!')
+    else:
+        st.error('No video stream found for the selected resolution.')
