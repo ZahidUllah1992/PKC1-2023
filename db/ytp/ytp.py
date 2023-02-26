ce5bf50785ee2d8c31d09d935c1a0c1f50bc946a
Usama Bajwa <usamabajwa86@gmail.com>
2023-02-27 00:47:44 +0500

    Update ytp.py

--- db/ytp/ytp.py
+++ db/ytp/ytp.py
@@ -1,15 +1,12 @@
 import os
 import streamlit as st
 from pytube import Playlist, Stream
-import zipfile
 
 def download_video(stream, title):
     with st.spinner(f'Downloading {title}...'):
         video_path = stream.download()
     st.success(f'{title} downloaded successfully!')
     return video_path
-
-from zipfile import ZipFile
 
 def download_all_videos(playlist_url, resolution):
     playlist = Playlist(playlist_url)
@@ -20,13 +17,8 @@ def download_all_videos(playlist_url, resolution):
             if stream:
                 video_path = download_video(stream, video.title)
                 video_paths.append(video_path)
-    # Define the path and filename for the zip file
-    zip_file_path = os.path.join(os.path.expanduser('~'), 'Downloads', f'{playlist.title}.zip')
-    with ZipFile(zip_file_path, 'w') as zip_file:
-        for video_path in video_paths:
-            # Add each video file to the zip file
-            zip_file.write(video_path, os.path.basename(video_path))
-    return zip_file_path
+    return video_paths
+
 st.title('YouTube Playlist Downloader')
 
 playlist_url = st.text_input('Enter the URL of the YouTube playlist:')
@@ -48,17 +40,19 @@ if st.button('Download All Videos'):
     playlist = Playlist(playlist_url)
     if playlist:
         download_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
-        zip_path = download_all_videos(playlist_url, resolution)
-        if zip_path:
-            st.download_button(
-                label='Click to download all videos',
-                data=open(zip_path, 'rb').read(),
-                file_name=os.path.basename(zip_path),
-                mime='application/zip',
-                # suggest a default download location
-                # this location is only a suggestion and the user can still choose a different location
-                # depending on their browser settings
-                folder=download_folder
-            )
+        video_paths = download_all_videos(playlist_url, resolution)
+        with st.spinner(f'Creating ZIP file...'):
+            zip_file_path = os.path.join(download_folder, f'{playlist.title}.zip')
+            with ZipFile(zip_file_path, 'w') as zip_file:
+                for video_path in video_paths:
+                    zip_file.write(video_path, os.path.basename(video_path))
+        st.success(f'All videos downloaded and saved to {zip_file_path}.')
+        st.info('Click on the button below to download the ZIP file.')
+        st.download_button(
+            label='Download ZIP file',
+            data=zip_file_path,
+            file_name=f'{playlist.title}.zip',
+            mime='application/zip'
+        )
     else:
         st.warning('Could not load playlist. Please try again later.')
