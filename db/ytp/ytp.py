12f6d0173ecd60a0ed441a1bd16985d8afddcbde
Usama Bajwa <usamabajwa86@gmail.com>
2023-02-26 19:36:39 +0500

    Update ytp.py

--- db/ytp/ytp.py
+++ db/ytp/ytp.py
@@ -10,13 +10,16 @@ def download_video(stream, title):
 
 def download_all_videos(playlist_url, resolution):
     playlist = Playlist(playlist_url)
+    downloaded_videos = []
     with st.spinner(f'Downloading {playlist.title}...'):
         for video in playlist.videos:
             stream = video.streams.filter(res=resolution).first()
             if stream:
                 download_video(stream, video.title)
+                downloaded_videos.append(video.title)
+    return downloaded_videos
 
-st.title('YouTube Playlist Downloader by Codanics')
+st.title('YouTube Playlist Downloader')
 
 playlist_url = st.text_input('Enter the URL of the YouTube playlist:')
 if not playlist_url.startswith('https://www.youtube.com/playlist?'):
@@ -34,5 +37,10 @@ resolutions = [
 resolution = st.selectbox('Select video resolution:', [res['label'] for res in resolutions])
 
 if st.button('Download All Videos'):
-    download_all_videos(playlist_url, resolution)
+    downloaded_videos = download_all_videos(playlist_url, resolution)
     st.success('All videos downloaded successfully!')
+
+if downloaded_videos:
+    st.write('Downloaded videos:')
+    for video in downloaded_videos:
+        st.write(video)
