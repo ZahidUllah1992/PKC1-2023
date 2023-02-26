86ac6d36826a2e56e427a2f564abf53fa8885c9d
Usama Bajwa <usamabajwa86@gmail.com>
2023-02-27 02:00:38 +0500

    Update ytp.py

--- db/ytp/ytp.py
+++ db/ytp/ytp.py
@@ -48,32 +48,9 @@ if downloaded_videos:
         if os.path.exists(video['file_path']):
             with open(video['file_path'], 'rb') as f:
                 video_bytes = f.read()
-            st.video(video_bytes)
+            st.video(video['file_path'])
             b64 = base64.b64encode(video_bytes).decode()
             href = f'<a href="data:file/mp4;base64,{b64}" download="{video["title"]}.mp4">Download {video["title"]}</a>'
             st.markdown(href, unsafe_allow_html=True)
-            download_file(video, 'video')
         else:
             st.warning(f'{video["title"]} does not exist.')
-
-def download_file(video, fmt):
-    """ Download the specified format for a video """
-    if fmt == 'audio':
-        stream_final = video.streams.get_audio_only()
-    else:
-        stream_final = video.streams.get_highest_resolution()
-    
-    if 'DESKTOP_SESSION' not in os.environ:
-        with st.spinner(f'Downloading {stream_final.title}...'):
-            file_path = stream_final.download()
-        st.success(f'{stream_final.title} downloaded successfully!')
-        
-        with open(file_path, 'rb') as f:
-            bytes = f.read()
-            b64 = base64.b64encode(bytes).decode()
-            href = f'<a href="data:file/zip;base64,{b64}" download=\'{stream_final.title}.{stream_final.subtype}\'>\
-                Download {stream_final.title}.{stream_final.subtype} \
-            </a>'
-            st.markdown(href, unsafe_allow_html=True)
-
-        os.remove(file_path) 
