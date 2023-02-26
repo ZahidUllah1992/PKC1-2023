00b9095e824bc40f273db86a7712820105f474c7
Usama Bajwa <usamabajwa86@gmail.com>
2023-02-27 02:20:47 +0500

    rr

--- db/ytp/ytp.py
+++ db/ytp/ytp.py
@@ -2,6 +2,7 @@ import os
 import streamlit as st
 from pytube import Playlist, Stream
 import base64
+import rarfile
 
 def download_video(stream, title):
     download_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
@@ -9,24 +10,7 @@ def download_video(stream, title):
         file_path = stream.download(output_path=download_folder)
     st.success(f'{title} downloaded successfully!')
     return file_path
-def download_file(stream, fmt):
-    if fmt == 'audio':
-        title = stream.title + '_audio.'+ stream.subtype
-    else:
-        title = stream.title + '.'+ stream.subtype
 
-    stream.download(filename=title)
-    
-    if 'DESKTOP_SESSION' not in os.environ:
-        with open(title, 'rb') as f:
-            bytes = f.read()
-            b64 = base64.b64encode(bytes).decode()
-            href = f'<a href="data:file/mp4;base64,{b64}" download=\'{title}\'>\
-                Here is your link \
-            </a>'
-            st.markdown(href, unsafe_allow_html=True)
-
-        os.remove(title)
 def download_all_videos(playlist_url, resolution):
     playlist = Playlist(playlist_url)
     downloaded_videos = []
@@ -60,15 +44,15 @@ if st.button('Download All Videos'):
     st.success('All videos downloaded successfully!')
 
 if downloaded_videos:
-    st.write('Downloaded videos:')
-    video_bytes = b''
-    for video in downloaded_videos:
-        if os.path.exists(video['file_path']):
-            with open(video['file_path'], 'rb') as f:
-                video_bytes += f.read()
-            os.remove(video['file_path'])
-        else:
-            st.warning(f'{video["title"]} does not exist.')
-    b64 = base64.b64encode(video_bytes).decode()
-    href = f'<a href="data:file/mp4;base64,{b64}" download="videos.zip">Download All Videos</a>'
-    st.markdown(href, unsafe_allow_html=True)
+    with rarfile.RarFile('videos.rar', 'w') as rar:
+        for video in downloaded_videos:
+            if os.path.exists(video['file_path']):
+                rar.write(video['file_path'], arcname=video['title'])
+                os.remove(video['file_path'])
+            else:
+                st.warning(f'{video["title"]} does not exist.')
+    with open('videos.rar', 'rb') as f:
+        bytes = f.read()
+        b64 = base64.b64encode(bytes).decode()
+        href = f'<a href="data:application/x-rar-compressed;base64,{b64}" download=\'videos.rar\'>Download All Videos</a>'
+        st.markdown(href, unsafe_allow_html=True)
