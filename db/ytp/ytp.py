cfb91f8687030028c649ba5c914268388131d2bd
Usama Bajwa <usamabajwa86@gmail.com>
2023-02-28 01:14:05 +0500

    Update ytp.py

--- db/ytp/ytp.py
+++ db/ytp/ytp.py
@@ -1,32 +1,11 @@
 import os
 import streamlit as st
 from pytube import Playlist, Stream
-import base64
-
-st.title('YouTube Playlist Downloader by Codanics')
-
-playlist_url = st.text_input('Enter the URL of the YouTube playlist:')
-ok_button = st.button('OK')
-
-resolutions = [
-    {'label': '1080p', 'value': '1080p'},
-    {'label': '720p', 'value': '720p'},
-    {'label': '480p', 'value': '480p'},
-    {'label': '360p', 'value': '360p'},
-    {'label': '240p', 'value': '240p'},
-    {'label': '144p', 'value': '144p'}
-]
-
-resolution = st.selectbox('Select video resolution:', [res['label'] for res in resolutions])
-
-downloaded_videos = []
 
 def download_video(stream, title):
-    download_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
     with st.spinner(f'Downloading {title}...'):
-        file_path = stream.download(output_path=download_folder)
+        stream.download()
     st.success(f'{title} downloaded successfully!')
-    return file_path
 
 def download_all_videos(playlist_url, resolution):
     playlist = Playlist(playlist_url)
@@ -34,18 +13,39 @@ def download_all_videos(playlist_url, resolution):
         for video in playlist.videos:
             stream = video.streams.filter(res=resolution).first()
             if stream:
-                file_path = download_video(stream, video.title)
-                downloaded_videos.append({'title': video.title, 'file_path': file_path})
-    return downloaded_videos
+                download_video(stream, video.title)
+
+st.title('YouTube Playlist Downloader')
+
+playlist_url = st.text_input('Enter the URL of the YouTube playlist:')
+ok_button = st.button('OK')
 
 if ok_button:
     if not playlist_url.startswith('https://www.youtube.com/playlist?'):
         st.warning('Please enter a valid YouTube playlist URL.')
         st.stop()
 
-if st.button('Download All Videos'):
-    downloaded_videos = download_all_videos(playlist_url, resolution)
-    if downloaded_videos:
+    resolutions = [
+        {'label': '1080p', 'value': '1080p'},
+        {'label': '720p', 'value': '720p'},
+        {'label': '480p', 'value': '480p'},
+        {'label': '360p', 'value': '360p'},
+        {'label': '240p', 'value': '240p'},
+        {'label': '144p', 'value': '144p'}
+    ]
+
+    resolution = st.selectbox('Select video resolution:', [res['label'] for res in resolutions])
+
+    if st.button('Download All Videos'):
+        download_all_videos(playlist_url, resolution)
         st.success('All videos downloaded successfully!')
-    else:
-        st.warning('No videos found in the playlist.')
+
+    if st.button('Download Videos'):
+        download_all_videos(playlist_url, resolution)
+        download_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
+        downloaded_videos = os.listdir(download_folder)
+        st.write('Downloaded videos:')
+        for video in downloaded_videos:
+            if video.endswith('.mp4'):
+                href = f'<a href="Downloads/{video}" download="{video}">Download {video}</a>'
+                st.markdown(href, unsafe_allow_html=True)
